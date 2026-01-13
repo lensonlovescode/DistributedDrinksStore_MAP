f717bb59e97a2420e6bc9d4322addebb4a09702b
lensonlovescode <lensonisinparis@gmail.com>
2026-01-13 17:20:31 +0300

    Structure Upload

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import routes from './routes/index';
+import redisClient from './utils/redis';
+import dbClient from './utils/db';
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`);
+});
